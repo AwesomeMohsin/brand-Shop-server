71f544cba1f45941d5cadd33bb6a742b3775efa8
The Awesome <mohsindude5@gmail.com>
2023-10-18 04:17:09 +0600

    new product added to database

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ require('dotenv').config()
 
 
 // middleware
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
 
-
+ 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ldqphqx.mongodb.net/?retryWrites=true&w=majority`;
@@ -28,8 +28,25 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const brandCollection = client.db('brandDB').collection('products')
+
+
+    // add a new product to database
+    app.post('/products', async (req, res) => {
+      const newProduct = req.body;
+      console.log(newProduct);
+      const result = await brandCollection.insertOne(newProduct);
+      res.send(result)
+    })
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
+    
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
